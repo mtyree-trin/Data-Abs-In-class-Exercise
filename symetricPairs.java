9735c00264adc681080a02b589ea9ee557495346
Molly Tyree <mollytyree@Mollys-MacBook-Pro-2.local>
2024-10-03 10:31:33 -0500

    10/1

--- symetricPairs.java
+++ symetricPairs.java
@@ -1,4 +1,16 @@
+import java.util.HashMap;
+
 public class symetricPairs {
     
+    HashMap<Integer,Integer> hm = new Hash
+    for(int i = 0; i < arr.length; i++){
+        int first = arr [i] [0];
+        int sec = arr [i] [1];
+        Integer val = hm.get(see);
+        if (val != null && val == first){
+            System.out.println(see + " " + first);
+        }else
+            hm.put(first, see);
+    }
 
 }
